f2a1691004c6ae0ba4beb68274082d63d7c87c55
Stephan Khoury and Viktor Grom <stephan+viktor@>
2016-11-28 13:47:16 -0800

    first test for ping pong

---
+++ Objects/pingPong.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace pingPong
+{
+  public class pingPong
+{
+  public bool IspingPong (int number)
+  {
+    return false;
+  }
+}
+}
+
+
+
+
+
+  /*Console.WriteLine("Enter number");
+   string yourNumber = Console.ReadLine();
+   int number  = int.Parse(yourNumber);
+
+   for(int i = 1; i <= number; i++ )
+   {
+     if (i%5 == 0 && i%3 == 0)
+     {
+       Console.WriteLine("Pingpong");
+     } else if (i%3 == 0 )
+     {
+     Console.WriteLine("Pong");
+   } else if (i%5 == 0) {
+       Console.WriteLine("Ping");
+     } else {
+       Console.WriteLine(i);
+     }
+    }
+   }
+  }*/
---
+++ pingpongTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+namespace pingPong
+{
+  public class pingPongTest
+  {
+    [Fact]
+    public void IspingPong_Pong()
+    {
+
+    }
+
+    }
+  }
